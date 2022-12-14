7f41fd3863af0f58a1b4cfa97adda0cfe4f44b3c
YaroshUlad <yarosh.uladzimir@gmail.com>
2022-12-15 00:14:16 +0300

    hw5 predeploy

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -16,11 +16,8 @@ function Pages() {
         <div>
             {/*Routes выбирает первый подходящий роут*/}
             <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
                 <Route  path='/' element={<PreJunior />} />
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
                 <Route  path={PATH.PRE_JUNIOR} element={<PreJunior />}/>
                 <Route  path={PATH.JUNIOR} element={<Junior />}/>
                 <Route  path={PATH.JUNIOR_PLUS} element={<JuniorPlus />}/>
--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -10,6 +10,10 @@ type PropsType = {
 }
 
 export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
+
+    
+    const location = window.location.hash.substring(1)
+
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
     return (
@@ -31,7 +35,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={location === PATH.PRE_JUNIOR ? s.active : ''}
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +43,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={location === PATH.JUNIOR ? s.active : ''} 
                     >
                         Junior
                     </NavLink>
@@ -47,7 +51,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={location === PATH.JUNIOR_PLUS ? s.active : ''} 
                     >
                         Junior Plus
                     </NavLink>
